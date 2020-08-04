b4531ee412ee0610974b53cf905ce3f34d56a8b6
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-08-04 19:25:11 -0400

    Edits

--- config.py
+++
@@ -1,8 +0,0 @@
-# Twitter API Credentials
-consumer_key = "akB4I4tteNdNGYy80W00MNqpm"
-consumer_secret = "LC6IfhlcHV4x7jqKVddFLNeZnDcqfQRZdyqM2uk3n5JnSh8NdY"
-access_token = "706617120277319680-ch0Tb8O7dkr4tOoNyzNz4k82jDppWDm"
-access_token_secret = "26H4iiO1hbCY1PRUTjRjdJPD2JRiv5oJfsnrcNCnaFiBS"
-
-# OpenWeatherMap API Key
-weather_api_key = '173a3b3361e3c0495b877c5090590d3e'
